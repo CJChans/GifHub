5615805af9d3c3cb7f71b80908b7e64b90ffcaca
CJChans <52377346+CJChans@users.noreply.github.com>
2019-08-05 20:41:41 +0800

    8.5点击盒子变宽--随机颜色

---
+++ my GifHub/utils.js
@@ -0,0 +1,44 @@
+/** 
+ * 
+ * @desc 求指定范围随机整数的方法
+ * @function
+ * @name randomInt 
+ * @param {number} min - 随机区间的最小值
+ * @param {number} max - 随机区间的最大值
+ * @returns {number} 随机整数
+ * */
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+/**
+ * 
+ * @desc 获取随机rgb颜色的方法
+ * @function
+ * @name randomRGBColor 
+ * @returns {string} rgb随机颜色
+ */
+function randomRGBColor() {
+    var r = randomInt(0, 255);
+    var g = randomInt(0, 255);
+    var b = randomInt(0, 255);
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+/**
+ * 
+ * @desc 获取随机十六进制颜色字符串的方法
+ * @function
+ * @name randomHexColor 
+ * @returns {string} 十六进制颜色字符串
+ */
+function randomHexColor() {
+    var hexColorChars = '0123456789abcdef';
+    var color = ['#'];
+    for (var i = 0; i < 6; i++) {
+        var index = Math.floor(Math.random() * hexColorChars.length);
+        color.push(hexColorChars[index]);
+    }
+    return color.join('');
+}
