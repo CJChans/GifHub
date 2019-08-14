9a0a2d15c1492bf919c3b624a246418401543853
CJChans <52377346+CJChans@users.noreply.github.com>
2019-08-14 08:40:33 +0800

    8.14旋转木马

--- my GifHub/16-旋转木马/js/demo.js
+++ my GifHub/16-旋转木马/js/demo.js
@@ -37,48 +37,22 @@ $(function() {
         }
     ];
 
-    //  旋转木马的思路
-    // 1. 让每个li标签根据配置单里面对应的数组元素生成各自的样式
-    // 2. 单击按钮的时候，改变配置里面的元素位置
-    // 3. 让每个li标签根据新的配置单重新生成各自的位置
-    // 4. 加节流阀  
-
-    // 把config数组中的每一个对象里面的属性，给对应的每一个li标签设置一遍，然后观察变化
-
     // 1. 获取标签对象
     var lis = $('#wrap .slide li');
-    var arrLeft = $('#arrLeft')
-    var arrRight = $('#arrRight')
-        // var timeId = null;
-
-    // 2. 鼠标移入大盒子的时候，要显示左右按钮
-    $('#wrap').hover(function() {
-        clearInterval(timeId)
-        $('#arrow').animate({
-            'opacity': 1
-        }, 300)
-    }, function() {
-        $('#arrow').animate({
-            'opacity': 0
-        }, 300)
-        timeId = setInterval(function() {
-            config.push(config.shift());
-            assign();
-        }, 1500)
-    })
+    var left = $('#arrLeft')
+    var right = $('#arrRight')
 
     function assign() {
-        // 3. 让每个li标签根据配置单中的数据进行一个样式呈现   因为配置单中的属性是用来控制标签的
         lis.each(function(index, ele) {
-            // $(ele).css(config[index])  // 让循环到的每一个标签拥有对应的数组中的样式  也就是每个li都有自己的样式
-            // $(ele).animate(config[index],500)
-            // 先设置一个层级,再设置一个动画的效果
             $(ele).css('zIndex', config[index].zIndex).stop().animate(config[index], 500)
-
         })
     }
-    assign(); // 
-    // 4.给右侧按钮注册事件 当单击右侧按钮的时候，要让所有的标签都移动一下
+    assign();
+    $('#wrap').hover(function() {
+        $('#arrow').animate({ opacity: 1 }, 300)
+    }, function() {
+        $('#arrow').animate({ opacity: 0 }, 300)
+    })
     $('#arrRight').on('click', function() {
         config.push(config.shift());
         assign();
@@ -87,8 +61,4 @@ $(function() {
         config.unshift(config.pop());
         assign();
     })
-    timeId = setInterval(function() {
-        config.push(config.shift());
-        assign();
-    }, 1500)
 })
